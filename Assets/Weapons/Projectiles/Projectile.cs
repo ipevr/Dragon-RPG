9bbae83fcb1849d028723071aee165f30406cc9a
INGO-PC\Ingo <i.peters@t-online.de>
2018-03-18 13:04:36 +0100

    New asset structure, horse asset imported

---
+++ Assets/Weapons/Projectiles/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed = 10f; // other classes can set speed
+    public float maxRange = 20f;
+
+    float damageCaused;
+    Vector3 startingPosition;
+
+    private void Start() {
+        startingPosition = transform.position;
+    }
+
+    private void Update() {
+        float range = (transform.position - startingPosition).magnitude;
+        if (range >= maxRange) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetDamage(float damage) {
+        damageCaused = damage;
+    }
+    
+    void OnCollisionEnter(Collision collision) {
+        Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+        if (damageableComponent) {
+            (damageableComponent as IDamageable).TakeDamage(damageCaused);
+            Destroy(gameObject);
+        }
+    }
+
+}
