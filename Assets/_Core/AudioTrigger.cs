e26643164bd7c444caa303f920ee90cce2f446e2
Ingo Peters <ingo.peters2@gmx.de>
2018-10-09 22:10:44 +0200

    - added audio sounds and voices

---
+++ Assets/_Core/AudioTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace RPG.Core {
+    public class AudioTrigger : MonoBehaviour {
+        [SerializeField] AudioClip clip;
+        [SerializeField] float audioVolume = 1f;
+        [SerializeField] int layerFilter = 0;
+        [SerializeField] float triggerRadius = 5f;
+        [SerializeField] bool isOneTimeOnly = true;
+
+        [SerializeField] bool hasPlayed = false;
+        AudioSource audioSource;
+
+        void Start() {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            audioSource.clip = clip;
+            audioSource.volume = audioVolume;
+
+            SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
+            sphereCollider.isTrigger = true;
+            sphereCollider.radius = triggerRadius;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.layer == layerFilter) {
+                RequestPlayAudioClip();
+            }
+        }
+
+        void RequestPlayAudioClip() {
+            if (isOneTimeOnly && hasPlayed) {
+                return;
+            } else if (!audioSource.isPlaying) {
+                audioSource.Play();
+                hasPlayed = true;
+            }
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.color = new Color(255f, 255f, 255f, .5f);
+            Gizmos.DrawWireSphere(transform.position, triggerRadius);
+        }
+    }
+}
